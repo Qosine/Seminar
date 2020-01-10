ca951be4a648f84e93874a7679567cad575973e7
MarcStam <59562366+MarcStam@users.noreply.github.com>
2020-01-10 14:47:38 +0100

    Add files via upload

---
+++ logit.R
@@ -0,0 +1,43 @@
+#Logit Base model - full model
+logit <- function(y,X){
+  blogit <- glm(y ~ X, data= data, family = "binomial")
+  return(summary(blogit))
+}
+#Weighted Logit Base model - Weights
+Weights <- function(data,CPS_target,gender,upper, lower){
+  CPS_nontarget <- 1-CPS_target
+  data$target <- ifelse(data$sd_gender == gender &  data$sd_age<=upper & data$sd_age>=lower, 1,0)
+  sample_weight_target <- sum(data$target)/nrow(data)
+  sample_weight_nontarget <- 1-sample_weight_target
+  Weights <- ifelse(data$sd_gender == gender &  data$sd_age<=upper & data$sd_age>=lower , CPS_target/sample_weight_target,CPS_nontarget/sample_weight_nontarget)
+  return(Weights)
+}
+#actual Weighted Logit
+Wlogit <- function(y, X, Weights){
+  blogit <- glm(y ~ X, data= data, family = "binomial", weights = Weights)
+  return(summary(blogit))
+}
+# lasso Logit
+Lasso_logit <- function(y, X){
+  RMSE <- function(y, yhat){(mean((y - yhat)^2))^.5}
+  lasso_logit.cv <- cv.glmnet(X, y, alpha = 1,
+                              standardize = FALSE, family = "binomial", intercept = TRUE, nfolds = 10)
+  lasso_logit <- glmnet(X, y, alpha = 1,
+                        standardize = FALSE, family = "binomial", intercept = TRUE) # path only for training data
+  plot(lasso_logit, xvar = "lambda", label = TRUE)
+  
+  
+  lasso_logit.best <- glmnet(X, y, alpha = 1,
+                             lambda = lasso_logit.cv$lambda.min,
+                             standardize = FALSE, family = "binomial",
+                             intercept = TRUE)
+  round(lasso_logit.best$beta, digits = 2)
+  lasso_logit_fit <- predict(lasso_logit.best, X)
+  RMSE(y, lasso_logit_fit)
+  blogit <- glm(y ~ X, family = "binomial")
+  logit_fit <- predict.glm(blogit, as.data.frame(X))
+  RMSE(y, logit_fit)
+  
+  cvfit <- glmnet::cv.glmnet(X, y)
+  return(coef(cvfit, s = "lambda.min"))
+}
