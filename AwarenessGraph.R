8fe8bc728f3fae60600b3180f373b6a430078799
qosine <pmail.kouwenhoven@gmail.com>
2020-01-10 09:38:01 +0100

    Update AwarenessGraph.R

--- AwarenessGraph.R
+++ AwarenessGraph.R
@@ -19,8 +19,9 @@ AwarenessGraph = function(data){
   #take only the dates, not the time
   data$id_date <- substr(data$id_date, 1, 10)
   
+  ##'Digital Audio'###########################################################
   
-  #sum 'digital Audio' by date
+  #sum 'Digital Audio' by date
   dSumOfDigAudio <- aggregate(data$mc_digitalaudio_all, by=list(Date=data$id_date), FUN=sum)
   dSumOfDigAudio = dSumOfDigAudio[2]
   
@@ -31,11 +32,53 @@ AwarenessGraph = function(data){
   dfDataDigAud$vUniqueDates = as.Date(dfDataDigAud$vUniqueDates)
   
   #plotting the mc_digital_audio_all versus time
-  plot(dfDataDigAud, main = 'Weighted sum of mc_digital_audio_al versus time', 
-       xlab = 'Time', ylab = 'Weighted sum' )  #plot shows 3 outliers which makes the rest of the plot unreadable
+  plot(dfDataDigAud, main = 'Weighted sum of mc_digital_audio_all versus time', 
+       xlab = 'Survey (2019)', ylab = 'Weighted sum' )  #plot shows 3 outliers which makes the rest of the plot unreadable
   
   #remove outliers of the weighted sum of the mc_digital_audio_all rows and plot again
   dfDataDigAud <- dfDataDigAud[dfDataDigAud$x<200,]
-  plot(dfDataDigAud, main = 'Weighted sum of mc_digital_audio_al versus time', 
-       xlab = 'Time', ylab = 'Weighted sum' ) 
+  plot(dfDataDigAud, main = 'Weighted sum of mc_digitalaudio_all versus time', 
+       xlab = 'Survey (2019)', ylab = 'Weighted sum' ) 
+  
+  ##'Online Display'###########################################################
+  
+  #sum 'Online Display' by date
+  dSumOfOnlDisp <- aggregate(data$mc_onlinedisplay_all, by=list(Date=data$id_date), FUN=sum)
+  dSumOfOnlDisp = dSumOfOnlDisp[2]
+  
+  #weight the sum over number of obs's in group (date)
+  dWeightedSumOnlDisp = dSumOfOnlDisp/iCount
+  #create df with the 
+  dfDataOnlDisp = data.frame(vUniqueDates, dWeightedSumOnlDisp)
+  dfDataOnlDisp$vUniqueDates = as.Date(dfDataOnlDisp$vUniqueDates)
+  
+  #plotting the mc_digital_audio_all versus time
+  plot(dfDataOnlDisp, main = 'Weighted sum of mc_onlinedisplay_all versus time', 
+       xlab = 'Survey (2019)', ylab = 'Weighted sum' )  #plot shows 3 outliers which makes the rest of the plot unreadable
+  
+  #remove outliers of the weighted sum of the mc_onlinedisplay_all rows and plot again
+  dfDataOnlDisp <- dfDataOnlDisp[dfDataOnlDisp$x<200,]
+  plot(dfDataOnlDisp, main = 'Weighted sum of mc_onlinedisplay_all versus time', 
+       xlab = 'Survey (2019)', ylab = 'Weighted sum') 
+  
+  ##'TV NBC'###########################################################
+  
+  #sum 'Online Display' by date
+  dSumOfTvNBC <- aggregate(data$mc_tv_nbc, by=list(Date=data$id_date), FUN=sum)
+  dSumOfTvNBC  = dSumOfTvNBC [2]
+  
+  #weight the sum over number of obs's in group (date)
+  dWeightedSumTvNBC  = dSumOfTvNBC /iCount
+  #create df with the 
+  dfDataTvNBC  = data.frame(vUniqueDates, dWeightedSumTvNBC)
+  dfDataTvNBC$vUniqueDates = as.Date(dfDataTvNBC$vUniqueDates)
+  
+  #plotting the mc_digital_audio_all versus time
+  plot(dfDataTvNBC, main = 'Weighted sum of mc_tv_nbc versus time', 
+       xlab = 'Survey (2019)', ylab = 'Weighted sum' )  #plot shows 3 outliers which makes the rest of the plot unreadable
+  
+  #remove outliers of the weighted sum of the mc_onlinedisplay_all rows and plot again
+  dfDataTvNBC <- dfDataTvNBC[dfDataTvNBC$x<10,]
+  plot(dfDataTvNBC, main = 'Weighted sum of mc_onlinedisplay_all versus time', 
+       xlab = 'Survey (2019)', ylab = 'Weighted sum') 
 }
